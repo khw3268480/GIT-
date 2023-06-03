e1ff8c1903ebf1d88a58fb850d9b177d292602a2
khw3268480 <khw3268480@naver.com>
2023-06-03 19:02:11 +0900

    Update 94.py

--- 94.py
+++ 94.py
@@ -1,1 +1,15 @@
-print("Hello Worlds!")
+a= []
+
+while True:
+    try:
+        a.append(input())
+    except:
+        break
+
+for i in range(len(a)):
+    for s in special_char:
+        a[i].replace(s, "")
+
+    print(a[i])
+
+
